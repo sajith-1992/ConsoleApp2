6e2e2d58f8af369a483d0c76b8b4fbba9f95ffba
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2025-03-18 12:08:52 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+namespace ConsoleApp2;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int myvalue = 5;
+
+        double myname = 10.5;
+        double final = myvalue + myname;
+
+
+       
+        Console.WriteLine("Hello,"+final);
+    }
+}
